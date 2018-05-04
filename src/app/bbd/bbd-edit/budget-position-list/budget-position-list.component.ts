8c4d412f34583a5a35affc2a3b84395f2de2b87e
kapluha <kapluhaanry@gmail.com>
2018-05-04 14:24:35 +0300

    Изменил везде BudgetBaseDocument на BBD
    Signed-off-by: kapluha <kapluhaanry@gmail.com>

---
+++ src/app/bbd/bbd-edit/budget-position-list/budget-position-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-position-list',
+  templateUrl: './budget-position-list.component.html',
+  styleUrls: ['./budget-position-list.component.css']
+})
+export class BudgetPositionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

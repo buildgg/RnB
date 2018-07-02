b889003dc138b873422903659cc1823fbdc5e1d0
tokar <tokar@csltd.com.ua>
2018-07-02 16:52:02 +0300

    better one

--- src/app/admin-page/user/user-routing.module.ts
+++ src/app/admin-page/user/user-routing.module.ts
@@ -3,7 +3,6 @@ import {AuthGuard} from '../../auth/auth-guard.service';
 import {Routes, RouterModule} from '@angular/router';
 import {UserComponent} from './user.component';
 import {UserLinkRoleEditFormComponent} from './user-link-role-edit-form/user-link-role-edit-form.component';
-import {UserLinkRoleListComponent} from "./user-link-role-list/user-link-role-list.component";
 import {UserLinkRoleViewComponent} from "./user-link-role-view/user-link-role-view.component";
 
 export const routesAdminPageUser: Routes = [
